9c0bd199ec11b0578e6eebcaaec6823a71e47822
Smit Patel <CS219-user@lab000000.cqdhyqslc1pedihwldg3213dfg.ux.internal.cloudapp.net>
2023-01-25 21:43:29 +0000

    Grade Calculator project after event handling was added.

---
+++ src/application/GradeCalculatorController.java
@@ -0,0 +1,60 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class GradeCalculatorController {
+
+
+	@FXML
+    private ChoiceBox<Integer> requiredcodingChallengesChoiceBox;
+	
+	@FXML
+	private ChoiceBox<Integer> optionalcodingChallengesChoiceBox;
+
+    @FXML
+    private Slider quizSlider;
+
+    @FXML
+    private TextField projectGradeTextfield;
+    
+    @FXML
+    private Label courseGradeLabel;
+
+    @FXML
+    void calculateGrade(ActionEvent event) {
+    	double courseGrade = 0.0;
+    	
+    	// assuming that the project is worth 50% towards the course grade
+    	String projectGrade = projectGradeTextfield.getText();
+    	courseGrade= courseGrade + Double.parseDouble(projectGrade) * 50/100;
+    	System.out.println("Project grade entered: " + projectGrade +
+    			" Course grade so far: " + courseGrade);
+    	// assuming that the quizzes is worth 25% towards the course grade
+    	// assuming that quizzes are marked out of 10.
+    	double quizGrade = quizSlider.getValue();
+    	courseGrade += (quizGrade *100/10 )* .25;
+    	System.out.println("Quiz grade entered: " + quizGrade +
+    			" Course grade so far: " + courseGrade);
+    	
+    	// assuming that the coding challenges are worth 25% towards the course grade
+    	// assuming that there are 15 required coding challenges
+    	int requiredcodingChallengesPassed = requiredcodingChallengesChoiceBox.getValue();
+    	courseGrade += (requiredcodingChallengesPassed*100/15)* .125;
+    	System.out.println("Coding challenges passed: " + requiredcodingChallengesPassed +
+    			" Course grade so far: " + courseGrade);
+    	// assuming that there are 5 optional coding challenges
+    	int optionalcodingChallengesPassed = optionalcodingChallengesChoiceBox.getValue();
+    	courseGrade += (optionalcodingChallengesPassed*100/5)* .125;
+    	
+    	// Display	result of calculation to the user in the window
+    	// Display result to two digits after decimal point
+    	courseGradeLabel.setText(String.format("Your overall course grade is: %.2f" , courseGrade));
+    	
+    }
+
+}
