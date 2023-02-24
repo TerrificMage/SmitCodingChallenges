9ddbe3f2dcd0aa79f519d9e0b109677c9659e783
SmitPatel <CS219-user@10.0.0.117>
2023-02-24 22:45:13 +0000

    Implemented the code and changes.

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,15 +1,21 @@
 package basicjava;
 
+import java.util.Arrays;
+
 public class CCArrays {
 
 	public static void replace(char[] charArray, char toReplace, char replaceWith) {
 		// TODO Auto-generated method stub
-		
+		for(int i =0; i<charArray.length;i++ ) {
+			if(charArray[i] ==Character.toUpperCase(toReplace) || charArray[i]==Character.toLowerCase(toReplace)) {
+				charArray[i]=replaceWith;
+			}
+		}
 	}
 
 	public static void sortAlphabetic(String[] strArray) {
 		// TODO Auto-generated method stub
-		
+		Arrays.sort(strArray, String::compareToIgnoreCase);
 	}
 
 }
